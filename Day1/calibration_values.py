34589f36fe44bf97a8db279aaedc8d9624b78411
Sara <sara.mccarney@mps-data.co.uk>
2024-01-08 20:52:10 +0000

    Fix bugs

--- Day1/calibration_values.py
+++ Day1/calibration_values.py
@@ -3,36 +3,36 @@ import sys
 
 class Calibration:
 
-    class CalibrationError:
-        raise Exception
-
     @staticmethod
-    def get_lines(file: BytesIO) -> str:
-        lines = [line.append('') for line in file]
+    def get_lines(filename: str) -> str:
+        file = open(filename, 'r')
+        lines = [line.replace('\n', '') for line in file]
         return lines
 
     @classmethod
     def get_calibration_value(cls, line: str) -> int:
-        int_chars = [int(char) for char in line]
-        for int_char in enumerate(idx, int_chars):
-            if int_char is not null:
-                first_digit = line[idx]
-            else:
-                raise CalibrationError
-        for int_char in enumerate(idx, int_chars[:-1]):
-            if int_char is not null:
-                last_digit = line[idx]
-            else:
-                raise CalibrationError
+        for char in line:
+            try:
+                int(char)
+                first_digit = char
+                break
+            except ValueError:
+                pass
+        for char in line[::-1]:
+            try:
+                int(char)
+                last_digit = char
+                break
+            except ValueError:
+                pass
         calibration_str = first_digit + last_digit
         calibration_value = int(calibration_str)
-
         if not isinstance(calibration_value, int):
             raise CalibrationError
         return calibration_value
 
     @classmethod
-    def sum_calibration_values(cls, calibration_values: Iterable) -> int:
+    def sum_calibration_values(cls, calibration_values: list) -> int:
         summed_value = 0
         for calibration_value in calibration_values:
             summed_value += calibration_value
@@ -40,7 +40,7 @@ class Calibration:
 
 
 if __name__ == '__main__':
-    streamable_file = sys.argv[0]
+    streamable_file = sys.argv[1]
     lines = Calibration.get_lines(streamable_file)
     calibration_values = [Calibration.get_calibration_value(line) for line in lines]
     sum_of_calibration_values = Calibration.sum_calibration_values(calibration_values)
